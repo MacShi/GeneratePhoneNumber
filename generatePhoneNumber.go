dfb47509521e916b13532484744cf8aef39d9022
test@test.com <test@test>
2021-05-02 22:20:48 +0800

    add the conut of generate phone number

--- generatePhoneNumber.go
+++ generatePhoneNumber.go
@@ -28,23 +28,20 @@ func batchGenerate(first string,count int64)  {
 	for i =0; i<count;i++{
 		phone := generatePhoneNumber(first)
 		wirterPhoneNumber(phone)
-		//rate :=fmt.Sprintf("%.2f", (float64(i+1)/10000000))
 		info := fmt.Sprintf("[Info] %s	进度%.2f%%	已完成%d", time.Now().Format("2006-01-02 15:04:05"), (float64((i+1)/count) *100 ),(i+1))
 		fmt.Println(info)
 	}
 }
 
 func main()  {
-	//fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	//fmt.Println(generatePhoneNumber("134"))
 	var first string
 	var count int64
 	flag.StringVar(&first,"f","","Top three mobile phone numbers, for example 135 136")
 	flag.Int64Var(&count,"c",10000000,"The count of generate phone number, default 10000000")
-	//flag.i
 	flag.Parse()
 	if len(first)==0{
-		fmt.Println("please use -h")
+		info :="Usage of gen_phone_windos.exe:\n  -c int\n        The count of generate phone number, default 10000000 (default 10000000)\n  -f string\n        Top three mobile phone numbers, for example 135 136"
+		fmt.Println(info)
 		os.Exit(1)
 	}
 	batchGenerate(first,count)
